b7acbaa86bf6ccaf520c0c72db2969aa0d3846f3
kwongwangjae <kgj9906@naver.com>
2024-03-23 22:54:44 +0900

    feat: 로그인 기초 설계

---
+++ backend/src/main/java/com/example/backend/domain/config/security/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.example.backend.domain.config.security;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Autowired
+	private OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return  http
+			.csrf(AbstractHttpConfigurer::disable)
+			.cors(cors -> cors.configurationSource(corsConfiguration()))
+			.authorizeHttpRequests(auth -> {
+			auth.anyRequest().authenticated();
+		})
+			.oauth2Login(oath2->{
+				oath2.successHandler(oAuth2LoginSuccessHandler);
+			})
+			.build();
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfiguration() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+		configuration.addAllowedHeader("*");
+		configuration.addAllowedMethod("*");
+		configuration.setAllowCredentials(true);
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**",configuration);
+		return urlBasedCorsConfigurationSource;
+	}
+}
